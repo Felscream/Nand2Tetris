26220449dcda4cf9d8955299c55b9cbb8ac0c7c8
Felscream <xblackfieldx@gmail.com>
2025-07-12 13:23:13 +0200

    Project 4 - mult & fill

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -8,4 +8,79 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+// total screen registers
+@8192
+D=A
+@R0
+M=D
+
+// current screen register index
+@registerIdx
+M=0
+
+// loop through all screen registers, one at a time
+(LOOP)
+  // read keyboard
+  // if KBD == 0, go to black
+  @KBD
+  D=M
+  @BLACK
+  D;JEQ
+  // else go to white
+  @WHITE
+  D;JNE
+
+// paint current screen register to black
+(BLACK)
+  @SCREEN
+  D=A
+  @registerIdx
+  A=D+M
+  M=0
+  // go to next register
+  @NEXT
+  0;JMP
+
+// paint curent scren register to white
+(WHITE)
+  @SCREEN
+  D=A
+  @registerIdx
+  A=D+M
+  M=-1
+  // go to next register
+  @NEXT
+  0;JMP
+
+// go to next screen register, 
+// loop back to first register if reached en
+(NEXT)
+  @R0
+  D=M
+  @registerIdx
+  D=D-M
+  // reset registerIdx if (R0 - registerIdx) <= 0
+  @RESET
+  D;JLE
+  // increment registerIdx if (R0 - registerIdx) > 0
+  @INC
+  D;JGT
+
+// set registerIdx to 0
+(RESET)
+  @registerIdx
+  M=0
+  @LOOP
+  0;JMP
+
+// increment registerIdx
+(INC)
+  @registerIdx
+  M=M+1
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
+
--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -7,4 +7,42 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+  // n = 0
+  @n
+  M=0
+  @R2
+  M=0
+  
+  // go to END if R0 = 0
+  @R0
+  D=M
+  @END
+  D;JEQ
+
+  // while n < R1, R2 = R2 + R0
+(LOOP)
+  // if n>= R1, go to END
+  @n
+  D=M
+  @R1
+  D=D-M
+  @END
+  D;JGE
+
+  // R2 = R2 + R0
+  @R0
+  D=M
+  @R2
+  D=D+M
+  M=D
+
+  //n = n + 1
+  @n
+  M=M+1
+
+  //goto loop
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
